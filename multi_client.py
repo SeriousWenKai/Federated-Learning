5c85624846faa37ef352000c7a2f02926f79e770
hanyiwen <hanyiwen@tju.edu.cn>
2018-07-22 16:33:45 +0800

    pycharm

--- multi_client.py
+++ multi_client.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
         p = multiprocessing.Process(target=start_client)
         jobs.append(p)
         p.start()
-        print("debug")
+        print("de")
     # TODO: randomly kill
